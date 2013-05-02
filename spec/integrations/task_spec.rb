928b3a00adeb9d435a8ea6183bded257c036d8bc
Pamela Overman <pwnela@gmail.com>
2013-05-02 10:39:47 -0400

    User can mark a task as complete (and also revert the task to incomplete)

--- spec/integrations/task_spec.rb
+++ spec/integrations/task_spec.rb
@@ -29,4 +29,35 @@ describe 'list' do
     page.should have_content "List Name"
     page.should have_content "Helicopter lesson"
   end
+
+   it 'allows a user to mark a task as completed' do
+    list = List.create(name: "List Name")    
+    excited_task = Task.create(description: "Get excited")
+    make_task = Task.create(description: "Make things")    
+    list.add_task(excited_task)
+    list.add_task(make_task)    
+
+    visit list_path(list)
+    click_link excited_task.description
+    check "task_completed"
+    click_button "Update Task"
+    page.should_not have_css("del", text: make_task.description)
+    page.should have_css("del", text: excited_task.description)
+  end
+
+  it 'allows a user to mark an incomplete task as not yet completed' do
+    list = List.create(name: "List Name")    
+    excited_task = Task.create(description: "Get excited")
+    make_task = Task.create(description: "Make things")    
+    list.add_task(excited_task)
+    list.add_task(make_task)  
+    excited_task.mark_complete  
+
+    visit list_path(list)
+    click_link excited_task.description
+    uncheck "task_completed"
+    click_button "Update Task"
+    page.should_not have_css("del", text: excited_task.description)
+  end
+
 end
