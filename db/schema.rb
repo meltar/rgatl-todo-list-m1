731295aeae339e572ed521a744bfe3249d7c799b
Pamela Overman <pwnela@gmail.com>
2013-05-01 21:11:20 -0400

    Adds the list_id column to the tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130502005112) do
+ActiveRecord::Schema.define(:version => 20130502010848) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20130502005112) do
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "list_id"
   end
 
 end
