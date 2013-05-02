ff8463d71482eb8688bdbe61008122d1097960ae
Pamela Overman <pwnela@gmail.com>
2013-05-01 21:00:36 -0400

    Requires a valid task to have a description

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   attr_accessible :description
+  validates :description, :presence => true
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,4 +5,9 @@ describe Task do
     task = Task.new(description: "Test Task")
     task.description.should eql "Test Task"
   end
+
+  it 'is not valid if it does not have a description' do
+    task = Task.new
+    task.valid?.should == false
+  end
 end
