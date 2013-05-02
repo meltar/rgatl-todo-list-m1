39a1c188ff4450a912c6489cc26bb33f5ac2bbc7
Pamela Overman <pwnela@gmail.com>
2013-05-02 11:11:04 -0400

    Adding a comment to explain the task to list association

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,10 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+    # We will use a hidden field in tasks/new.html.erb to pass the task's list id to the create action.
+    # This is how the task is associated to the right list.
+    # params[:list_id] is populated in the link constructed on lines 6 and 8 in lists/show.html.erb
+    # (Note: there is a better way to do this. Check out "build" on http://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html.)
     @list = List.find(params[:list_id])
   end
 
